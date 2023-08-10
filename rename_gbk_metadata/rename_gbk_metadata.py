5f42251f85fd98f9402b78e44d512ee133bbb46f
Jorge <jorge.c.navarro.munoz@gmail.com>
2023-08-10 17:24:08 +0200

    rename_gbk_metadata: v1.1. Cleanup and added option for external dictionary

--- rename_gbk_metadata/rename_gbk_metadata.py
+++ rename_gbk_metadata/rename_gbk_metadata.py
@@ -2,8 +2,10 @@
 
 """
 Renames some genbank metadata (LOCUS, DEFINITION, ACCESSION) that start with 
-generic 'contig' or 'scaffold' strings and inserts the filename
-It also updates the date
+generic 'contig' or 'scaffold' strings and inserts the filename.
+Optionally, the string can be replaced entirely with some other string (see
+option --external).
+It also updates the date.
 """
 
 from pathlib import Path
@@ -13,7 +15,7 @@ from Bio import SeqIO
 from datetime import date
 
 __author__ = "Jorge Navarro"
-__version__ = "1.0"
+__version__ = "1.1"
 __maintainer__ = "Jorge Navarro"
 __email__ = "jorge.navarromunoz@wur.nl"
 
@@ -54,6 +56,16 @@ def parameter_parser():
         help="Updates date in GenBank headers to today"
     )
 
+    parser.add_argument(
+        "-e",
+        "--external",
+        help="Provide an external dictionary for the string substitution. \
+            It should be a tab-separated file with two columns: the first being\
+            the base filename (from e.g. [BASE FILENAME].region001.gbk) and the\
+            second, the string to use for replacement in the metadata.",
+        type=Path
+    )
+
     return parser.parse_args()
 
 
@@ -88,8 +100,36 @@ def collect_files(input_folder):
     return paths_files
 
 
+def parse_dict_file(external_dict_file:Path) -> dict:
+    """
+    Reads and parses a two column tsv file with
+    Col1 = base filename in the input folders
+    Col2 = string to use in metadata (e.g. a short label, strain etc)
+    """
+
+    original_to_substitute = {}
+    with open(external_dict_file) as edf:
+        for line in edf:
+            if line[0] == "#":
+                continue
+            try:
+                original, substitute = line.strip().split("\t")
+            except ValueError:
+                exit(f"Something went wrong while trying to read line\n{line}")
+            else:
+                original_to_substitute[original] = substitute
+
+    if len(original_to_substitute) == 0:
+        exit("Error: no data found in the external dictionary file")
+
+    print(f"Found an external dictionary file with {len(original_to_substitute)} substitutions")
+
+    return original_to_substitute
+
+
 def modify_metadata(
-    gb_file: Path, base_output_folder: Path, verbose=False, update_date=False
+    gb_file: Path, base_output_folder: Path, substitute_strings: dict, 
+    verbose=False, update_date=False,
 ) -> bool:
     """
     Opens and scans a genbank file, inserting the filename in front of the
@@ -101,6 +141,7 @@ def modify_metadata(
     """
 
     filename = gb_file.stem
+    prefix = filename.split(".region")[0]
     header_fixed = False
 
     if base_output_folder is not None:
@@ -118,13 +159,22 @@ def modify_metadata(
         for record in records:
             # record.id     modifies *both* ACCESSION and VERSION
             # record.name   modifies LOCUS
-            if not record.id.startswith(filename):
-                record.id = f"{filename}_{record.id}"
+
+            # Attempt to forcibly replace string if external dictionary provided
+            if substitute_strings and prefix in substitute_strings:
+                record.id = f"{substitute_strings[prefix]}"
+                record.name = f"{substitute_strings[prefix]}"
                 header_fixed = True
 
-            if not record.name.startswith(filename):
-                record.name = f"{filename}_{record.name}"
-                header_fixed = True
+            # else, simply append filename at the beginning
+            else:
+                if not record.id.startswith(filename):
+                    record.id = f"{filename}_{record.id}"
+                    header_fixed = True
+
+                if not record.name.startswith(filename):
+                    record.name = f"{filename}_{record.name}"
+                    header_fixed = True
 
             today = date.today().strftime('%d-%b-%Y').upper()
             if update_date and record.annotations["date"] != today:
@@ -138,48 +188,21 @@ def modify_metadata(
             print(output_file)
 
 
-def modify_in_place(gb_file: Path, verbose=False) -> bool:
-    """
-    Opens and scans a genbank file, inserting the filename to the header if
-    it starts with 'contig'
-    The original file is overwritten with the modified data
-
-    Output (bool): whether any header was fixed
-    """
-
-    header_fixed = False
-
-    modified_data = []
-    with open(gb_file) as fasta:
-        fasta_stem = gb_file.stem
-        for line in fasta:
-            if line[0] == ">":
-                if line[1:].lstrip().lower().startswith("contig"):
-                    header_fixed = True
-                    modified_data.append(f">{fasta_stem}_{line[1:].lstrip()}")
-                    continue
-
-            modified_data.append(line)
-
-    with open(gb_file, "w") as fasta:
-        fasta.write("".join(modified_data))
-
-    if verbose:
-        print(gb_file)
-
-    return header_fixed
-
-
 def main():
     parameters = parameter_parser()
 
     # collect files
     file_list = collect_files(parameters.inputfolder)
 
+    # optionally, look for an external dictionary file
+    substitute_strings = {}
+    if parameters.external:
+        substitute_strings = parse_dict_file(parameters.external)
+
     # modify files if necessary
-    for fasta_file in sorted(file_list):
-        modify_metadata(fasta_file, parameters.outputfolder, parameters.verbose,
-                        parameters.update_date)
+    for gb_file in sorted(file_list):
+        modify_metadata(gb_file, parameters.outputfolder, substitute_strings, 
+                        parameters.verbose, parameters.update_date)
 
 
 if __name__ == "__main__":
